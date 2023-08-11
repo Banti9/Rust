0ea9feb93d1cccefc1f0abcba5af5dbe67138d99
Banti jk <110710466+Banti9@users.noreply.github.com>
2023-08-11 23:33:04 +0530

    commit

---
+++ hello.rs
@@ -0,0 +1,13 @@
+fn main(){
+    println!("HELLO WORlD");
+    // This is my name
+    print!("Banti");
+    println!(" Chauhan");
+    // my net warth
+    println!("Integer");
+    print!("{}",09270);
+    print!("B");
+    /*uyug6tfrtdesrvgtfct
+    knhytygvv
+    ju */
+}
